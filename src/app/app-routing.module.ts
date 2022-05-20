f247099654006d1d9453be28dafaac944c2630b1
Lucas Henrick <lucashenrikc@gmail.com>
2022-05-20 14:19:32 -0300

    Adição na rota para exibir o list card episodio e css no menu.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { EpisodioListComponent } from './componets/episodio-list/episodio-list.component';
 import { PersoInfoComponent } from './componets/perso-info/perso-info.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path:"character/:id",
     component:PersoInfoComponent
+  },
+  {
+    path:"episodios",
+    component:EpisodioListComponent
   }
 ];
 
