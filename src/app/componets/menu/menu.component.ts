e455820aa45f1ef29e3792ddb6cdfb314418a4fe
Lucas Henrick <lucashenrikc@gmail.com>
2022-05-24 20:49:43 -0300

    Alteração par ao menu responsivo

--- src/app/componets/menu/menu.component.ts
+++ src/app/componets/menu/menu.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  public active: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public openSidenave(): void {
+    this.active = this.active !== '' ? ''  :'active';
   }
 
 }
